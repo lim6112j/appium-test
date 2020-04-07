9456cd11ffa61339817dfb38ea40cc3a9d17d532
lim byeong cheol <lim6112j@gmail.com>
2020-04-07 18:24:26 +0900

    ui tests made

--- index.js
+++ index.js
@@ -7,27 +7,63 @@ const opts = {
   port: 4723,
   path: "/wd/hub/",
   capabilities: {
-    platformName: "Android",
-    platformVersion: "8",
-    deviceName: "Android Emulator",
-    app: "/Users/byeongcheollim/Downloads/ApiDemos-debug.apk",
-    appPackage: "io.appium.android.apis",
-    appActivity: ".view.TextFields",
-    automationName: "UiAutomator2"
+    "platformName": "Android",
+    "platformVersion": "8",
+    "deviceName": "Android Emulator",
+    "app": "/Users/byeongcheollim/Downloads/app-beta-release.apk",
+    "appWaitPackage": "com.deforav2.beta",
+    "automationName": "UiAutomator2",
+    "appWaitDuration": 50000,
+    "noReset": true,
+    "appWaitActivity": "com.deforav2.MainActivity"
   }
 };
 
 async function main () {
   //   let driver = new UiAutomator2.AndroidUiautomator2Driver();
   // await driver.createSession(opts.capabilities);
+
   const client = await wdio.remote(opts);
+  client.setImplicitTimeout(30000);
+  let el1 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.TextView[1]");
 
 
-  const field = await client.$("android.widget.EditText");
-  await field.setValue("Hello World!");
-  const value = await field.getText();
-  assert.equal(value,"Hello World!");
+  const value = await el1.getText();
 
+  assert.equal(value, "Enter PIN");
+
+
+  // pinEle.waitForVisible();
+  await client.pressKeyCode(7); // presss 0 key
+  await client.pressKeyCode(7);
+  await client.pressKeyCode(7);
+  await client.pressKeyCode(7);
+  await client.pressKeyCode(7);
+  await client.pressKeyCode(7);
+  // const mobileEle = await client.$("#07bf9f41-c2d0-45b6-9a10-0c613bf20fb2")
+  let el2 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[1]/android.widget.TextView[2]");
+  el2.click();
+
+let el3 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[1]/android.widget.TextView[1]");
+el3.click();
+
+  // mobileEle.waitForVisible();
+  // mobileEle.click();
+  // assert.equal((await mobileEle).isSelected, true);
+  // const field = await client.$("android.widget.EditText");
+  // await field.setValue("Hello World!");
+  // const value = await field.getText();
+  // assert.equal(value,"Hello World!");
+  const el3_txt = await el3.getText();
+  assert(el3_txt, 'All');
+
+  let el4 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.TextView[3]");
+
+  el4.click();
+  // let el5 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[3]/android.widget.ImageView");
+  // el5.click();
+  // let el6 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[4]/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.TextView");
+  // el6.click();
   await client.deleteSession();
 }
 
