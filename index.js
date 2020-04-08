bd5acb71d8c47937f2dae7c75fdfdbe9664dfc3b
lim byeong cheol <lim6112j@gmail.com>
2020-04-08 14:59:02 +0900

    create group, activity test 완료

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const wdio = require("webdriverio");
-// const UiAutomator2 = require(`appium-uiautomator2-driver`);
+const readline = require('readline');
 const assert = require("assert");
-// javascript
 const opts = {
   hostname: "localhost",
   port: 4723,
@@ -20,51 +19,100 @@ const opts = {
 };
 
 async function main () {
-  //   let driver = new UiAutomator2.AndroidUiautomator2Driver();
-  // await driver.createSession(opts.capabilities);
-
   const client = await wdio.remote(opts);
-  client.setImplicitTimeout(30000);
+  // 앱 타임아웃 방지
+  await client.setImplicitTimeout(30000);
+  // simple test
   let el1 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.TextView[1]");
-
-
   const value = await el1.getText();
-
   assert.equal(value, "Enter PIN");
 
 
-  // pinEle.waitForVisible();
+  // Pin번호 입력.
   await client.pressKeyCode(7); // presss 0 key
   await client.pressKeyCode(7);
   await client.pressKeyCode(7);
   await client.pressKeyCode(7);
   await client.pressKeyCode(7);
   await client.pressKeyCode(7);
-  // const mobileEle = await client.$("#07bf9f41-c2d0-45b6-9a10-0c613bf20fb2")
-  let el2 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[1]/android.widget.TextView[2]");
-  el2.click();
 
-let el3 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[1]/android.widget.TextView[1]");
-el3.click();
-
-  // mobileEle.waitForVisible();
-  // mobileEle.click();
-  // assert.equal((await mobileEle).isSelected, true);
-  // const field = await client.$("android.widget.EditText");
-  // await field.setValue("Hello World!");
-  // const value = await field.getText();
-  // assert.equal(value,"Hello World!");
+  // all button text값 확인.
+  let el3 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[1]/android.widget.TextView[1]");
   const el3_txt = await el3.getText();
   assert(el3_txt, 'All');
 
-  let el4 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.TextView[3]");
+  // group 탭으로 이동
+  let groupBtn1 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[2]/android.widget.ImageView");
+  await client.touchAction({action: "tap", element: groupBtn1});
+  // 그룹생성 버튼 클릭
+  let createGroupBtn = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ImageView");
+  await client.touchAction({action: "tap", element: createGroupBtn});
+  let createGroupBtn2 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.TextView");
+  // createGroupBtn2.waitForClickable();
+  // createGroupBtn2.click();
+  await client.touchAction({action: "tap", element: createGroupBtn2});
+  
+  // 중복 그룹명 입력
+  let textInput = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.widget.EditText[1]");
+  await textInput.setValue(`test`);
+  let finishBtn = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.ImageView");
+  await client.touchAction({action: "tap", element: finishBtn});
+  
+  // 그룹명 재입력
+  let errTextField = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.widget.EditText");
+  await errTextField.setValue(`test-${Math.floor(Math.random() * 1000000000000)}`);
+  const finishBtn2 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.ImageView")
+  await client.touchAction({action: "tap", element: finishBtn2});
+  
+  // 그룹내 액티비티 생성
+  let createActivityBtn = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.ImageView");
+  await createActivityBtn.click();
+  // client.touchAction({action: "tap", element: createActivityBtn});
+  
+  // 리뷰 선택
+  const reviewCreate = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup");
+  await reviewCreate.click();
+  const next1 = await client.$택"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.ImageView");
+  await next1.click();
+  // 리뷰 타이틀 작성
+  const activityTitle = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText[1]");
+  await activityTitle.setValue(`Activity Title -${Math.floor(Math.random() * 1000000000000)}`)
+  const next2 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.widget.ImageView");
+  await next2.click();
+  // 날짜 선택 
+  const dateInput = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]")
+  await dateInput.click();
+  const future = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.DatePicker/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.NumberPicker[2]/android.widget.Button[2]")
+  await client.touchAction([
+    {action: 'press', element: future},
+    {action: 'moveTo', x: 542, y: 852},
+    'release'
+  ]);
+  const okBtn = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[2]")
+  await okBtn.click();
+  //  시간 선택... 디폴트로 ok
+  await okBtn.click();
+  const next3 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.widget.ImageView");
+  await next3.click();
+  const finishBtn3 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.widget.ImageView")
+  await finishBtn3.click();
+  // 리뷰 질문 하나 추가
+  const reviewField1 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.widget.EditText")
+  await reviewField1.setValue('test question')
+  // 리뷰 작성 끝 완료버튼 클릭.
+  const finishBtn4 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.widget.ImageView")
+  await finishBtn4.click();
 
-  el4.click();
-  // let el5 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[3]/android.widget.ImageView");
-  // el5.click();
-  // let el6 = await client.$("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[4]/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.TextView");
-  // el6.click();
-  await client.deleteSession();
+  // 종료 프로세스
+  readline.emitKeypressEvents(process.stdin);
+  process.stdin.setRawMode(true);
+  console.log(` ########## press any key for exit ##########`);
+  process.stdin.on('keypress', async (str, key) => {
+    if (key !== undefined) {
+      await client.deleteSession().catch(err => process.exit());
+      process.exit();
+    }
+  })
 }
 
 main();
---
+++ index2.js
@@ -0,0 +1,65 @@
+const wdio = require("webdriverio");
+const readline = require('readline');
+// const UiAutomator2 = require(`appium-uiautomator2-driver`);
+const assert = require("assert");
+// javascript
+const opts = {
+  hostname: "localhost",
+  port: 4723,
+  path: "/wd/hub/",
+  capabilities: {
+    "platformName": "Android",
+    "platformVersion": "8",
+    "deviceName": "Android Emulator",
+    "app": "/Users/byeongcheollim/Downloads/app-beta-release.apk",
+    "appWaitPackage": "com.deforav2.beta",
+    "automationName": "UiAutomator2",
+    "appWaitDuration": 50000,
+    "noReset": true,
+    "appWaitActivity": "com.deforav2.MainActivity"
+  }
+};
+
+async function main () {
+  //   let driver = new UiAutomator2.AndroidUiautomator2Driver();
+  // await driver.createSession(opts.capabilities);
+
+  const client = await wdio.remote(opts);
+  client.setImplicitTimeout(30000);
+  let selector = 'new UiSelector().text("Enter PIN").className("android.widget.TextView")';
+  let el1 = await client.$(`android=${selector}`)
+
+
+
+  const value = await el1.getText();
+
+  assert.equal(value, "Enter PIN");
+
+
+  // pinEle.waitForVisible();
+  await client.pressKeyCode(7); // presss 0 key
+  await client.pressKeyCode(7);
+  await client.pressKeyCode(7);
+  await client.pressKeyCode(7);
+  await client.pressKeyCode(7);
+  await client.pressKeyCode(7);
+
+  selector = 'new UiSelector().resourceId("com.deforav2.beta:id/58b7d112-d096-4a3c-bc84-209dce50a9f6").className("android.widget.ImageView")';
+  el1 = await client.$(`android=${selector}`)
+  el1.click()
+
+  // await client.touchAction({action: 'tap', x: 314, y: 1692})
+
+  readline.emitKeypressEvents(process.stdin);
+  process.stdin.setRawMode(true);
+  console.log(` ########## press any key for exit ##########`);
+  process.stdin.on('keypress', async (str, key) => {
+    if (key !== undefined) {
+      await client.deleteSession().catch(err => process.exit());
+      process.exit();
+    }
+  })
+  // await client.deleteSession();
+}
+
+main();
